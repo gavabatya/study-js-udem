51c274cde855a7df5373149d0532bf33bbd0a47e
gavabatya <79111180057@yandex.ru>
2023-02-27 19:48:35 +0300

    tasks js

---
+++ TASKS/06-data-types/finish.js
@@ -0,0 +1,30 @@
+/** ЗАДАЧА 6 - Типы данных
+ *
+ * 1. Объявите несколько переменных и присвойте им значения:
+ *  - строка
+ *  - число
+ *  - логическое
+ *  - null
+ *  - undefined
+ *  - объект
+ *  - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений
+ * используя оператор typeof
+ */
+
+let str = "string/строка";
+let num = 29;
+let bool = true;
+let noth = null;
+let und;
+let myObj = { sex: "Male" };
+let myArr = [29, "Igor", true];
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof bool);
+console.log(typeof noth);
+console.log(typeof und);
+console.log(typeof myObj);
+console.log(typeof myArr);
