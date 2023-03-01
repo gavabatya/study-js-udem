8754ea7e96d3d6f5c2e378778612107c31c6eba8
gavabatya <79111180057@yandex.ru>
2023-03-01 13:18:42 +0300

    add task 12

---
+++ TASKS/12-create-object/finish.js
@@ -0,0 +1,20 @@
+/** ЗАДАЧА 12 - Создание объекта
+ *
+ * 1. Создайте объект с тремя свойствами:
+ *  - name
+ *  - surname
+ *  - favoriteNumber
+ *
+ * 2. Выведите в консоль строку
+ * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
+ */
+
+let myObj = {
+  name: "Igor",
+  surname: "Gavrilov",
+  favoriteNumber: 29,
+};
+
+console.log(
+  `My name is ${myObj.name} ${myObj.surname} and my favorite number is ${myObj.favoriteNumber}`
+);
