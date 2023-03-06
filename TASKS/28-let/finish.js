60868bf62b316a1a97d30d9e911e8ee8c89d20f8
gavabatya <79111180057@yandex.ru>
2023-03-06 13:48:36 +0300

    add 21-29 tasks

---
+++ TASKS/28-let/finish.js
@@ -0,0 +1,25 @@
+/** ЗАДАЧА 28 - Let
+ *
+ * Измените код так, чтобы ошибка исчезла
+ */
+/*
+const myFavoriteAnimal = 'Monkey'
+
+console.log(myFavoriteAnimal)
+// 'Monkey'
+
+const myFavoriteAnimal = 'Cat'
+
+console.log(myFavoriteAnimal)
+// 'Cat'
+*/
+
+let myFavoriteAnimal = "Monkey";
+
+console.log(myFavoriteAnimal);
+// 'Monkey'
+
+myFavoriteAnimal = "Cat";
+
+console.log(myFavoriteAnimal);
+// 'Cat'
